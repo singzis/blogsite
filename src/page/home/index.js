7b7abd9de5dac11683788b965f7f175ed17b96a6
huajing <huajing@microants.cn>
2020-01-13 18:10:20 +0800

    feat:引入布局

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from "react";
+import { Layout } from "antd";
 import "./index.css";
+
+const { Footer, Sider, Content } = Layout;
 
 const Home = () => {
   const [data, setDate] = useState("home");
 
-  return <div>{data}</div>;
+  return (
+    <Layout>
+      <Sider style={{ background: '#fff' }}>Sider</Sider>
+      <Layout  style={{ background: '#fff' }}>
+        <Content>Content</Content>
+        <Footer  style={{ background: '#fff' }}>Footer</Footer>
+      </Layout>
+    </Layout>
+  );
 };
 
 export default Home;
