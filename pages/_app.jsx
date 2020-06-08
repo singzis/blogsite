9bdf04fac07c15195342518d9907f6f83e8432ea
zissou72 <599106418@qq.com>
2020-06-08 23:00:10 +0800

    feat:[id].js样式

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
 import "antd/dist/antd.css";
+import "highlight.js/styles/atom-one-dark.css";
 import "../assets/reset.css";
 import "../assets/styles.css";
 
--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,19 +1,35 @@
+import { useEffect } from "react";
 import Head from "next/head";
 import Layout from "../../components/layout";
 import Date from "../../components/date";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
 
-export default function Post({ postData: { title, text, date, contentHtml } }) {
+export default function Post({ postData: { title, date, contentHtml } }) {
+  useEffect(() => {
+    //   hljs.registerLanguage("javascript", javascript);
+    hljs.initHighlightingOnLoad();
+  }, [contentHtml]);
   return (
     <Layout>
       <Head>
         <title>{title}</title>
       </Head>
       <article>
-        <div>{title}</div>
+        <div className="title">{title}</div>
         <Date dateString={date} />
         <div dangerouslySetInnerHTML={{ __html: contentHtml }}></div>
       </article>
+      <style jsx>{`
+        .title {
+          text-align: center;
+          font-size: 42px;
+          font-weight: 500;
+          line-height: 1.5;
+          padding: 10px 0;
+        }
+      `}</style>
     </Layout>
   );
 }
