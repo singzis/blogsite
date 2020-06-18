4b9b99cc321a20595c57574d17a0418c293f780f
zissou72 <599106418@qq.com>
2020-06-18 23:54:22 +0800

    更新footer和header组件的名称，因为最初是用大写开头的，现在已经改不过来了

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Header from "./header";
-import Footer from "./footer";
+import Header from "./Header";
+import Footer from "./Footer";
 import Personal from "./personal";
 import BackToTop from "./backToTop";
 
